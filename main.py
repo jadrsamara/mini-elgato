17fcc0db3df0e82220e12cd27d04fff90fca5c02
Jad Samara <jadsamara@yahoo.com>
2023-12-15 22:33:56 +0200

    add send_love command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import sqlite3
 import requests
 import database
 import logger
+import random
 
 # * * * * * * * * * *  BOT SETTINGS  * * * * * * * * * *
 
@@ -447,6 +448,50 @@ async def list_delete(ctx, *, num):
 """
 Random commands
 """
+
+@bot.hybrid_command(name='send_love', description='Send love to someone special')
+async def send_love(ctx, *, user):
+
+    """
+    Send love to someone special.
+    """
+
+    user_valid = user.replace(' ', '').replace('<@', '').replace('>', '')
+    try:
+        int(user_valid)
+    except ValueError as error:
+        logger.warn(f'{ctx.message.author} gave wrong input: {user}', ctx)
+        await ctx.send('Please mention a user when trying to send love', error)
+        return
+
+    love_messages = [
+        "I love you", 
+        "I miss you", 
+        "I wish you were here", 
+        "My heart belongs to you", 
+        "All that I do, I do for you", 
+        "Thinking about you", 
+        "My world revolves around you", 
+        "You are mine and I am yours", 
+        "You are perfect for Me", 
+        "I will always be there for You", 
+        "I miss the taste of your lips, the scent of your hair, the feel of your skin.", 
+        "When I look into your eyes, I just know we were meant to be", 
+        "Life would be impossible without You", 
+        "You make me happy", 
+        "You are all that I need and will ever need", 
+        "Ours is a love so real, so true, so deep...", 
+        "I treasure every moment we spent together", 
+        "You are special", 
+        "You are mine to love and care for", 
+        "I appreciate You", 
+        "Without You, my world is empty"
+    ]
+    
+    await ctx.send(f'<@{user_valid}>, {love_messages[random.randint(0, len(love_messages))]} ❤️')
+    
+    logger.info(f'{ctx.message.author} sent love to \'{user}\'', ctx)
+
 
 @bot.hybrid_command(name='joke', description='Tells a lame Joke')
 async def joke(ctx):
